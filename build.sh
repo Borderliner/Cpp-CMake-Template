7a21573f7c35a28365fb95b1e9416ca8108837a1
Borderliner <hajianpour.mr@gmail.com>
2017-04-30 02:48:40 +0430

    Send info of config.yml from bash to CMake

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
-EXE_NAME="test2"
+#!/bin/bash
+source bin/parse_yaml
+eval $(parse_yaml config.yml "config_")
+EXE_NAME=$config_project_exe
 TYPE="$1"
 
 function debug-build() {
   rm -rf build/debug
   mkdir -p build/debug
   cd build/debug
-  cmake ../.. -DEXE_NAME:STRING=$EXE_NAME -DCMAKE_BUILD_TYPE=Debug
+  cmake ../.. \
+  -DAPPLICATION_NAME:STRING=$config_project_name \
+  -DAPPLICATION_EXE:STRING=$config_project_exe \
+  -DAPPLICATION_VERSION_MAJOR:STRING=$config_project_version_major \
+  -DAPPLICATION_VERSION_MINOR:STRING=$config_project_version_minor \
+  -DAPPLICATION_VERSION_PATCH:STRING=$config_project_version_patch \
+  -DAPPLICATION_VERSION_TYPE:STRING=$config_project_version_type \
+  -DAPPLICATION_VENDOR_ID:STRING=$config_project_vendor_id \
+  -DAPPLICATION_VENDOR_NAME:STRING=$config_project_vendor_name \
+  -DAPPLICATION_VENDOR_URL:STRING=$config_project_vendor_url \
+  -DCMAKE_BUILD_TYPE=Debug
   make -j4
 }
 
@@ -18,7 +31,17 @@ function release-build() {
   rm -rf build/release
   mkdir -p build/release
   cd build/release
-  cmake ../.. -DEXE_NAME:STRING=$EXE_NAME -DCMAKE_BUILD_TYPE=Release
+  cmake ../.. \
+  -DAPPLICATION_NAME:STRING=$config_project_name \
+  -DAPPLICATION_EXE:STRING=$config_project_exe \
+  -DAPPLICATION_VERSION_MAJOR:STRING=$config_project_version_major \
+  -DAPPLICATION_VERSION_MINOR:STRING=$config_project_version_minor \
+  -DAPPLICATION_VERSION_PATCH:STRING=$config_project_version_patch \
+  -DAPPLICATION_VERSION_TYPE:STRING=$config_project_version_type \
+  -DAPPLICATION_VENDOR_ID:STRING=$config_project_vendor_id \
+  -DAPPLICATION_VENDOR_NAME:STRING=$config_project_vendor_name \
+  -DAPPLICATION_VENDOR_URL:STRING=$config_project_vendor_url \
+  -DCMAKE_BUILD_TYPE=Release
   make -j4
 }
 
