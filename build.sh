5c656f0b34a593a1b4bd7f042f21b203dc4209a1
Borderliner <hajianpour.mr@gmail.com>
2017-04-30 00:36:35 +0430

    Add Release build mode

--- build.sh
+++ build.sh
@@ -5,13 +5,26 @@ function debug-build() {
   rm -rf build/debug
   mkdir -p build/debug
   cd build/debug
-  cmake ../.. -DEXE_NAME:STRING=$EXE_NAME
+  cmake ../.. -DEXE_NAME:STRING=$EXE_NAME -DCMAKE_BUILD_TYPE=Debug
   make -j4
 }
 
 function debug-run() {
   chmod +x build/debug/$EXE_NAME
   ./build/debug/$EXE_NAME
+}
+
+function release-build() {
+  rm -rf build/release
+  mkdir -p build/release
+  cd build/release
+  cmake ../.. -DEXE_NAME:STRING=$EXE_NAME -DCMAKE_BUILD_TYPE=Release
+  make -j4
+}
+
+function release-run() {
+  chmod +x build/release/$EXE_NAME
+  ./build/release/$EXE_NAME
 }
 
 function process-input() {
@@ -22,6 +35,14 @@ function process-input() {
     ;;
     dr|--debug-run)
     debug-run
+    shift
+    ;;
+    rb|--release-build)
+    release-build
+    shift
+    ;;
+    rr|--release-run)
+    release-run
     shift
     ;;
     *)
